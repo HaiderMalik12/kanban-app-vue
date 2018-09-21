66565d4fcf5c192b755fdf9ff146f2583a3cb03b
Haider Malik <haidermalik504@gmail.com>
2018-09-21 09:32:04 +0500

    compute active coloumn

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -2,7 +2,7 @@
     <div class="uk-flex-center">
         <div class="task-header">
             <div class="uk-card uk-card-default uk-card-small uk-card-body">
-                <h3 class="uk-card-title">New Todo</h3>
+                <h3 class="uk-card-title">New {{getColTitle}}</h3>
                 <div class="uk-margin">
                     <input class="uk-input uk-form-width-medium uk-form-small" type="text" placeholder="Add Task">
                 </div>
@@ -14,7 +14,13 @@
 </template>
 
 <script>
+import {store} from '../store.js'
 export default {
-    name: 'AddTask'
+    name: 'AddTask',
+    computed:{
+        getColTitle(){
+          return store.getActiveColumn().name
+        }
+    }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -2,5 +2,13 @@ import { Seed } from './seed';
 export const store = {
 	state: {
 		Seed
+	},
+	setActiveColumn(colId) {
+		this.state.Seed.map(
+			col => (col.id === colId ? (col.active = true) : (col.active = false))
+		);
+	},
+	getActiveColumn() {
+		return this.state.Seed.find(col => col.active === true);
 	}
 };
